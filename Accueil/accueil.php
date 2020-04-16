4c0feac7d7ec9dc9f769c97f7e6f3cbedd087d2a
FL <Jeremy.filin@hotmail.fr>
2020-04-16 15:21:45 +0200

    Include
    Include scrit header slider

--- Accueil/accueil.php
+++ Accueil/accueil.php
@@ -5,13 +5,13 @@
     <link href="https://fonts.googleapis.com/css?family=Oswald|Oxanium|Spartan&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="stylucss/style.css">
+    <link rel="stylesheet" href="CssJs/style.css">
     </head>
 <body>
-<?php include ('Header.php') ?>
-<?php include ('Slider.php') ?>
-
-<script src="script.js"></script>
+<?php include ('images')?>
+<?php include ('Header/Header.php') ?>
+<?php include ('Slidu/Slider.php') ?>
+<?php include ('Sriptu/script.php') ?>
 </body>
 </html>
 
