269c6f1fcb10ab5a593628183ac5185a741b29c1
FL <Jeremy.filin@hotmail.fr>
2020-04-16 15:11:45 +0200

    PHP
    J'ai tt changer en php

---
+++ Header/Header.php
@@ -0,0 +1,31 @@
+<header>
+    <img id="logo" src="tryheader3.png" alt="">
+    <div class="MyBurger">
+      <div class="burger-wrap">
+        <div class="burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+       </div>
+        <div class="mobile-menu">
+          <nav>
+            <ul>
+              <li><a href="#">PRODUITS</a></li>
+              <li><a href="#">SERVICES</a></li>
+              <li><a href="#">CONTACT</a></li>
+              <li><a href="#">QUI SOMMES NOUS ?</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <nav id="Barnav">
+      <ul>
+          <li><a href="#">PRODUITS</a></li>
+          <li><a href="#">SERVICES</a></li>
+          <li><a href="#">CONTACT</a></li>
+          <li><a href="#">QUI SOMMES NOUS ?</a></li>
+      </ul>
+  </nav>
+  </header>
