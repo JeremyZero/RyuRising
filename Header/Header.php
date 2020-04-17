56d613c3fd115b06441fb8815b8d32270cf7788d
Yukinamida <deliquaire.nagui@live.Fr>
2020-04-17 11:50:54 +0200

    modifheader

--- Header/Header.php
+++ Header/Header.php
@@ -1,31 +1,31 @@
 <header>
-    <img id="logo" src="../images/tryheader3.png" alt="">
-    <div class="MyBurger">
-      <div class="burger-wrap">
-        <div class="burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-       </div>
-        <div class="mobile-menu">
-          <nav>
-            <ul>
-              <li><a href="#">PRODUITS</a></li>
-              <li><a href="#">SERVICES</a></li>
-              <li><a href="#">CONTACT</a></li>
-              <li><a href="#">QUI SOMMES NOUS ?</a></li>
-            </ul>
-          </nav>
-        </div>
+    <img id="logo" src="../images/tryheader3.png" alt="">    
+  </header>
+  <nav id="Barnav">
+    <ul>
+        <li><a href="#">PRODUITS</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">CONTACT</a></li>
+        <li><a href="#">QUI SOMMES NOUS ?</a></li>
+    </ul>
+</nav>
+  <div class="MyBurger">
+    <div class="burger-wrap">
+      <div class="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+     </div>
+      <div class="mobile-menu">
+        <nav>
+          <ul>
+            <li><a href="#">PRODUITS</a></li>
+            <li><a href="#">SERVICES</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a href="#">QUI SOMMES NOUS ?</a></li>
+          </ul>
+        </nav>
       </div>
     </div>
-    <nav id="Barnav">
-      <ul>
-          <li><a href="#">PRODUITS</a></li>
-          <li><a href="#">SERVICES</a></li>
-          <li><a href="#">CONTACT</a></li>
-          <li><a href="#">QUI SOMMES NOUS ?</a></li>
-      </ul>
-  </nav>
-  </header>
+  </div>
