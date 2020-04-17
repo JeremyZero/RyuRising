67dd136bdf260eeb992906822ba8826ec6169783
FL <Jeremy.filin@hotmail.fr>
2020-04-17 11:40:15 +0200

    Modif

--- Accueil/accueil.php
+++ Accueil/accueil.php
@@ -5,22 +5,12 @@
     <link href="https://fonts.googleapis.com/css?family=Oswald|Oxanium|Spartan&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="CssJs/style.css">
-    </head>
-<body>
-<?php include ('images')?>
-<?php include ('Header/Header.php') ?>
-<?php include ('Slidu/Slider.php') ?>
-<?php include ('Sriptu/script.php') ?>
-=======
     <link rel="stylesheet" href="../CssJs/stylee.css">
     </head>
 <body>
 <?php include ('../Header/Header.php') ?>
 <?php include ('../Slidu/Slider.php') ?>
 <script src="../Scriptu/script.js"></script>
->>>>>>> master
 </body>
 </html>
 
