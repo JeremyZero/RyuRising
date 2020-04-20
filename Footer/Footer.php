9d9b6bdd321e009b06c48850823747d54f969b2d
Yukinamida <deliquaire.nagui@live.Fr>
2020-04-20 11:19:47 +0200

    AjoutFooter

---
+++ Footer/Footer.php
@@ -0,0 +1,28 @@
+<footer>
+  <div class="FuckingCenter">
+  <div class="Offres">
+    <h3>OFFRE: -20% SUR LE PACK</h3>
+    <a href="#"><img id="Shakerfooter"src="packshaker.png" alt=""></a>
+  </div>
+  <div class="AllCG">
+  <h3>CONDITIONS GÉNÉRALES</h3>
+  <ul class="CG">
+    <li><a class="AllFooter" href="#">Politique de confidentialité</a></li>
+    <li><a class="AllFooter" href="#">CGV</a></li>
+  </ul>
+</div>
+<div class="APropos">
+  <h3>LIVRAISON</h3>
+  <li><a class="AllFooter" href="#">Livraison</a></li>
+  <li><a class="AllFooter" href="#">Échanges et retours</a></li>
+</div>
+<div class="APropos">
+  <h3>À PROPOS</h3>
+  <li><a class="AllFooter" href="#">Suivre ma commande</a></li>
+  <li><a class="AllFooter" href="#">Devenir Ambassadeur</a></li>
+</div>
+</div>
+</footer>
+<div id="Copyright">
+Copyright© 2020 Ryu Rising. Tous Droits Réservés.
+</div>
