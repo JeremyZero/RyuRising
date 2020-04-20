619a2fabd1838d74d4d650818064be0c272e403e
FL <Jeremy.filin@hotmail.fr>
2020-04-20 14:52:11 +0200

    EC
    Echange & Retour

---
+++ EC.php
@@ -0,0 +1,20 @@
+<div class="footercat" >
+<h2>RETOURS</h2>
+
+
+<p>Étant donné le traitement rapide de nos commandes, tu n'as pas la possibilité d'annuler ou de modifier ta commande. 
+    <br> Tu peux en revanche nous réexpédier le colis contre remboursement dès réception.<br>
+
+Pour ce faire, nous  t'invitons à nous contacter par mail, par téléphone.<br>
+Nous t'enverrons par la suite une étiquette déjà remplie et franchisée à imprimer et coller sur ton colis à retourner.<br>
+
+Tu n'auras plus qu'à le déposer à la poste à la suite de quoi un remboursement total sera effectué dès réception du colis chez nous.<br>
+
+Une fois que nous aurons reçu et inspecté l’article retourné, nous t'enverrons un e-mail pour confirmer que nous l’avons bien reçu et nous t'informerons également de notre décision quant à l’approbation ou au rejet de ta demande de remboursement.<br>
+
+
+Notre politique est de 30 jours. Si 30 jours se sont écoulés depuis ton achat, nous ne pouvons malheureusement pas t'offrir un remboursement ou un échange.<br>
+
+Pour toutes questions, on reste joignable à cette adresse: info@ryurising.com
+</p>
+</div>
