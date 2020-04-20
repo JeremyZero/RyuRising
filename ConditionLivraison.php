78eb8ca489f535612c63fe47264b76824f0d331a
FL <Jeremy.filin@hotmail.fr>
2020-04-20 15:03:58 +0200

    PageLivraison
    La page livraison

---
+++ ConditionLivraison.php
@@ -0,0 +1,28 @@
+<div class="footercat" >
+<h2>LIVRAISON</h2>
+
+<p>Combien coûte la livraison?<br>
+Elle est gratuite pour une livraison standard (5 à 7 jours) et 5.97€ pour une livraison premium (24/48H avec suivi).<br>
+
+Quel transporteur utilisez-vous?<br>
+Nos colis sont livrés à domicile par Chronopost.   <br> 
+
+Où est mon colis?<br>
+Un numéro de suivi  t'est envoyé par mail 24H maximum après le passage de ta commande (pense à jeter un coup d'oeil dans tes spams).<br>
+
+Quels sont vos délais de traitement et de livraison?<br>
+Ton colis est confié à Chronopost 12H maximum après le passage de ta commande et te sera acheminé entre 24-48H et 5 à 7 jours selon ton choix de livraison (standard ou premium).<br>
+
+À quoi dois-je m'attendre au niveau de l'emballage?<br>
+Tes produits seront soigneusement emballés et calés à l'intérieur d'un carton aux couleurs de notre marque.<br>
+
+Et si je ne suis pas présent le jour de la livraison?<br>
+Pas de problème, tu recevras un mail te notifiant du passage du livreur et te laissant la possibilité de reprogrammer une livraison directement sur le site de Chronopost avec la date et l’horaire de ton choix. Tu peux également, à ce moment-là, choisir un point relais.<br>
+
+N'hésite pas à nous contacter et nous répondrons avec plaisir à toutes tes questions:<br>
+
+04.93.13.74.51 <br>
+info@ryurising.com<br>
+</p>
+
+</div>
