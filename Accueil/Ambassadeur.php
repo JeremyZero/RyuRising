9a41c16f064d0c74e9de388c3d93560d4b877380
Yukinamida <deliquaire.nagui@live.Fr>
2020-04-20 16:07:53 +0200

    Ajout Ambassadeur et modif PC + lien href

---
+++ Accueil/Ambassadeur.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Oxanium|Spartan&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CssJs/stylee.css">
+    </head>
+<body>
+<?php include ('../Header/Header.php') ?>
+<?php include ('../AmbassadeurInclude.php') ?>
+<?php include ('../Footer/Footer.php') ?>
+
+</body>
+</html>
+
---
+++ AmbassadeurInclude.php
@@ -0,0 +1,8 @@
+<div class="footercat">
+<h2>Tu veux devenir ambassadeur Ryu Rising ?</h2><br>
+ 
+
+ <p>Pour cela c'est très simple! Il te suffit de nous envoyer ta demande par email avec le maximum de détails et nous reviendrons vers toi très vite avec la marche à suivre pour que tu rejoignes l'aventure ;)</p><br>
+ 
+ <p>Contact: info@ryurising.com</p>
+</div>
