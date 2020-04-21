2b744a9bab6ceb0fd2058fc1d73ffc623ee2da98
Yukinamida <deliquaire.nagui@live.Fr>
2020-04-21 16:38:14 +0200

    ajout lien logo accueil

--- Include/Header/Header.php
+++ Include/Header/Header.php
@@ -1,5 +1,5 @@
 <header>
-    <img id="logo" src="../images/tryheader3.png" alt="">
+    <a href="../Index/Accueil.php"></a><img id="logo" src="../images/tryheader3.png" alt="">
     
   </header>
   <nav id="Barnav">
