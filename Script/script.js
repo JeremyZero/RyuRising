4b8094d5b1053a6b8c8ad6e3bdd0fc44574a6450
Yukinamida <deliquaire.nagui@live.Fr>
2020-05-05 11:11:47 +0200

    commentaire

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,7 @@
 var slideIndex = 0;
 showSlides();
 
+// Slideshow page d'accueil //
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -15,9 +16,10 @@ function showSlides() {
   // }
   slides[slideIndex-1].style.display = "block";  
   // dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3500); // Change image every 2 seconds
+  setTimeout(showSlides, 3500);
 };
 
+// Burger Responsive //
 burger = document.querySelector('.burger');
 burger_wrap = document.querySelector('.burger-wrap');
 mobileMenu = document.querySelector('.mobile-menu');
