918aae295bfc2876d0d4a841f25ba8abaac05361
Yukinamida <deliquaire.nagui@live.Fr>
2020-06-24 23:02:03 +0200

    w3c

--- Bassededonne/_Hearder.php
+++ Bassededonne/_Hearder.php
@@ -3,8 +3,8 @@ require '../Bassededonne/db.class.php';
 require '../Bassededonne/Panier.class.php';
 // J'initialise l'objet de db.class.php
 // cette fonction me permet de construire mon objet
-// si j'aimerais me connecter avec une nouvelle base de donner je remplie le DB('');
+// si je souhaite me connecter avec une nouvelle base de donner je remplie le DB('');
   $DB= new DB();  
-  // J'initilise ma class panier du ficher panier.class.php
+  // J'initialise ma class panier du ficher panier.class.php
   $panier = new panier($DB);
 ?>
--- Include/Slider/Slider.php
+++ Include/Slider/Slider.php
@@ -3,39 +3,39 @@
 
         <div class="mySlides fade">
           <div class="numbertext">1 / 6</div>
-          <img class="imgslide" src="../images/11.png" style="width:100%">
+          <img class="imgslide" src="../images/11.png" alt="Slide-Grimmjoy">
         </div>
         
         <div class="mySlides fade">
           <div class="numbertext">2 / 6</div>
-          <img class="imgslide" src="../images/22.png" style="width:100%">
+          <img class="imgslide" src="../images/22.png" alt="Slide-Nezuko">
         </div>
         
         <div class="mySlides fade">
           <div class="numbertext">3 / 6</div>
-          <img class="imgslide" src="../images/33.png" style="width:100%">
+          <img class="imgslide" src="../images/33.png" alt="Slide-Ryurinengan">
         </div>
         
         <div class="mySlides fade">
             <div class="numbertext">4 / 6</div>
-            <img class="imgslide" src="../images/44.png" style="width:100%">
+            <img class="imgslide" src="../images/44.png" alt="Slide-Evangelion">
           </div>
         
           <div class="mySlides fade">
             <div class="numbertext">5 / 6</div>
-            <img class="imgslide" src="../images/55.png" style="width:100%">
+            <img class="imgslide" src="../images/55.png" alt="Slide-RyuDLuffy">
           </div>
         
           <div class="mySlides fade">
             <div class="numbertext">6 / 6</div>
-            <img class="imgslide" src="../images/66.png" style="width:100%">
+            <img class="imgslide" src="../images/66.png" alt="Slide-Ulquiorrise">
           </div>
         
         </div>
 </div>
 
         <div id="Barrenoire">
-  <img id="TexteRyu" src="../images/ryurising.png" alt="">
+  <img id="TexteRyu" src="../images/ryurising.png" alt="Slogan-RyuRising">
 </div>
 
 
