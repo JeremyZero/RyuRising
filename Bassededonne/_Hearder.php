d935c1a6e1a32ecd747add96bb6040960317ebcb
FL <Jeremy.filin@hotmail.fr>
2020-06-24 15:34:16 +0200

    New commentaire

--- Bassededonne/_Hearder.php
+++ Bassededonne/_Hearder.php
@@ -1,9 +1,10 @@
 <?php
 require '../Bassededonne/db.class.php';
 require '../Bassededonne/Panier.class.php';
-// J'initialise mon objet cette fonction me permet de construire mon objet
-// DB Me sert a mettre des information de connection si j'aimerais me connecter a une autre base de données
-  $DB = new DB();  
+// J'initialise l'objet de db.class.php
+// cette fonction me permet de construire mon objet
+// si j'aimerais me connecter avec une nouvelle base de donner je remplie le DB('');
+  $DB= new DB();  
   // J'initilise ma class panier du ficher panier.class.php
   $panier = new panier($DB);
 ?>
